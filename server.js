f8a13b7d229781bb5d54239d03f7ef8d060e679a
Hector Sanchez <hsanchez7934@gmail.com>
2018-01-22 11:42:33 -0700

    Add static json file for heroku deployment

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.resolve(__dirname, './index.html')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.locals.title = 'Drinks';
+
+const requireHTTPS = (request, response, next) => {
+  if (request.header('x-forwarded-proto') !== 'https') {
+    return response.redirect(`https://${request.header('host')}${request.url}`);
+  }
+  return next();
+};
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(requireHTTPS);
+}
+
+app.get('/', (req, res) =>
+  res.sendFile(
+    path.resolve(__dirname, './index.html'
+    )
+  );
+);
+
+app.listen(app.get('port'), () => {
+  //eslint-disable-next-line
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
